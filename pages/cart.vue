e8a178c74b3fbea18b45224ce6dc31aded53d9b4
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-14 15:01:24 +0100

    added media queries

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container mx-auto mt-8 w-10/12 p-5">
+    <div class="container mx-auto mt-8 w-10/12 lg:p-5">
         <h1 class="text-xl">Shopping Cart</h1>
-        <div v-if="cartStore.itemCount > 0" class="flex items-start justify-between">
-            <div class="w-2/3 mr-36">
+        <div v-if="cartStore.itemCount > 0" class="flex flex-col lg:flex-row items-start justify-between">
+            <div class="w-full lg:w-2/3 mr-36">
                 <div v-for="item in cartStore.items" :key="item.id"
-                    class="flex items-center justify-start gap-5 w-full py-8 border-b">
+                    class="flex items-center justify-start gap-3 lg:gap-5 w-full py-8 border-b mb-5 lg:mb-0">
                     <img :src="item.image" alt="" class="h-[120px] w-[120px] object-cover">
                     <div class="w-full">
                         <div class="mb-10">
                             <div class="flex justify-between w-full">
-                                <p class="text-[#1E1F21] text-lg">{{ item.title.slice(0, 25) }}</p>
+                                <p class="text-[#1E1F21]  text-base lg:text-lg">{{ item.title.slice(0, 25) }}</p>
                                 <p>$20</p>
                             </div>
                             <div class="flex justify-between w-full">
@@ -18,7 +18,7 @@
                             </div>
                         </div>
                         <div class="flex justify-between w-full">
-                            <span>Qty:
+                            <span class="text-sm">Qty:
                                 <button class="ml-2">-</button>
                                 <span class="px-4 py-1 border mx-2">1</span>
                                 <button>+</button>
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <div class="p-4 border border-[#E8E8E8] rounded-md w-[350px]">
+            <div class="p-4 border border-[#E8E8E8] rounded-md w-full lg:w-[350px]">
                 <span class="text-[#4B4B4D] text-xs font-bold">SUMMARY</span>
                 <div class="my-4">
                     <div class="flex justify-between text-sm text-[#4B4B4D]">
@@ -51,7 +51,7 @@
                 <button class="btn my-4 p-2 w-full">Proceed to checkout</button>
             </div>
         </div>
-        <div v-else class="text-center w-full">Looks like your cart is empty</div>
+        <div v-else class="my-20 text-center w-full text-[#848484]">Looks like your cart is empty</div>
 
         <ProductsListing title="Top picks" />
 
